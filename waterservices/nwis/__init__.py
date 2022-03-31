cb624d6d137e8720a0000efb1e1e1bc447c2be21
javad <javad_design@yahoo.com>
2022-03-31 23:30:40 +0430

    remove: commented codes

--- waterservices/nwis/__init__.py
+++ waterservices/nwis/__init__.py
@@ -35,33 +35,11 @@ class NWIS:
                 tmp.writelines(all_lines)
                 tmp.flush()
                 tmp.seek(0)
-
                 
                 usecols = ['site_no', 'station_nm', 'dec_lat_va', 'dec_long_va', 'huc_cd', 'begin_date', 'end_date']
                 df_grouped = dd.read_csv(path, sep="\t", usecols=usecols).compute()
-                # df_grouped = df.groupby(['site_no', 'station_nm', 'dec_lat_va', 'dec_long_va'])\
-                #             .agg({'huc_cd': 'first', 'begin_date': 'first', 'end_date': 'first'}).compute().reset_index()
                 df_grouped.drop(df_grouped.loc[df_grouped['site_no']=='15s'].index, inplace=True)
                 df_grouped.drop(df_grouped.loc[df_grouped['site_no']=='site_no'].index, inplace=True)
                 df_grouped.to_csv(f'{self.csvFilename}.csv', index=False)
-
-                # # lst = open(path, "r").readlines()
-                # text_reader = csv.reader(tmp, delimiter='\t')
-                # with open(f'{self.csvFilename}.csv', "w", newline='') as csv_file:
-                #     writer = csv.writer(csv_file, delimiter=',')
-                #     writer.writerows(df)
-                #     # for line in lst:
-                #     #     writer.writerow(line)
-
-            
-            
-            # df_grouped = df.groupby(['site_no', 'station_nm', 'dec_lat_va', 'dec_long_va'])\
-            #     .agg({'huc_cd': 'first', 'begin_date': 'first', 'end_date': 'first'}).compute().reset_index()
-
-            # df_grouped.drop(df_grouped.loc[df_grouped['site_no']=='15s'].index, inplace=True)
-            # df_grouped.drop(df_grouped.loc[df_grouped['site_no']=='site_no'].index, inplace=True)
-            # df_grouped['date_length'] = df_grouped.apply(lambda x: ( int(str(x['end_date'])[:4]) - int(str(x['begin_date'])[:4]) ) + 1, axis=1)
-            # # df_grouped.to_csv(f'{self.csvFilename}.csv', index=False)
-
         finally:
             os.remove(path)
