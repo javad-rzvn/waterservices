b4fd1d5a282a9588584efe81a4d8e3567dc2e260
javad <javad_design@yahoo.com>
2022-04-05 12:41:22 +0430

    add: input col names // add: loop through filters // add: input filter list

--- waterservices/nwis/__init__.py
+++ waterservices/nwis/__init__.py
@@ -9,24 +9,34 @@ import dask.dataframe as dd
 import csv
 
 class NWIS:
-    def __init__(self, siteType, hucList, filters, csvFilename):
+    def __init__(self, siteType, hucList, filters, input_columns, csvFilename):
         self.siteType = siteType
         self.hucList = hucList
-        self.csvFilename = csvFilename
         self.filters = filters
+        self.input_columns = input_columns
+        self.csvFilename = csvFilename
 
     
     def siteInfo(self):
         fd, path = tempfile.mkstemp(suffix=".txt", prefix="tmp")
         all_lines = []
+
+        # convert filter from input dict to list and then convert all items in the list to one string
+        filters_list = []
+        for fkey, fvalue in self.filters.items():
+            url_filter = fkey + '=' + fvalue
+            filters_list.append(url_filter)
+        all_filters = '&'.join(map(str, filters_list))
+
         try:
+            
+            # get sites' info with huc code
             with os.fdopen(fd, 'r+') as tmp:
                 for huc in tqdm(self.hucList):
                     filter0 = 'format=rdb'
                     filter1 = f'&huc={huc}'
                     filter2 = f'&siteType={self.siteType}'
-                    filter3 = '&seriesCatalogOutput=true'
-                    URL = f"https://waterservices.usgs.gov/nwis/site/?{filter0}{filter1}{filter2}{filter3}"
+                    URL = f"https://waterservices.usgs.gov/nwis/site/?{filter0}{filter1}{filter2}&{all_filters}"
                     r = requests.get(URL)
 
                     string_to_add = ''
@@ -36,10 +46,12 @@ class NWIS:
                 tmp.flush()
                 tmp.seek(0)
                 
-                usecols = ['site_no', 'station_nm', 'dec_lat_va', 'dec_long_va', 'huc_cd', 'begin_date', 'end_date']
+                usecols = self.input_columns
                 df_grouped = dd.read_csv(path, sep="\t", usecols=usecols).compute()
                 df_grouped.drop(df_grouped.loc[df_grouped['site_no']=='15s'].index, inplace=True)
                 df_grouped.drop(df_grouped.loc[df_grouped['site_no']=='site_no'].index, inplace=True)
                 df_grouped.to_csv(f'{self.csvFilename}.csv', index=False)
         finally:
             os.remove(path)
+
+# TODO: get sites' info with list of site no
